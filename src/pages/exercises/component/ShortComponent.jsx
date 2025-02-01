176894c7ab1c3648266a39718668a5baadec3a31
RafaelVGomes <69604376+RafaelVGomes@users.noreply.github.com>
2025-02-01 17:44:37 -0300

    New exercises

---
+++ src/pages/exercises/component/ShortComponent.jsx
@@ -0,0 +1,5 @@
+const ShortComponent = () => {
+  return <p>Essa é a estrutura curta de um componente com arrow function.</p>
+}
+
+export default ShortComponent;
---
+++ src/pages/exercises/useState/ChangeColorExample.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import SelectBSColors from './SelectBSColors';
+
+const ChangeColorExample = () => {
+  const [color, setColor] = useState('primary');
+
+  const handleColorChange = (newColor) => {
+    setColor(newColor);
+  };
+
+  return (
+    <div className={`bg-${color} p-3`}>
+      <h1 className={`text-bg-${color}`}>Mude a minha cor!</h1>
+      <SelectBSColors onChange={handleColorChange} />
+    </div>
+  );
+};
+
+export default ChangeColorExample;
